a0279c0ff84e518b66d644b89e1cc59f67b4cfee
Chelsea Yue <cyue@lcs.on.ca>
2019-05-07 14:54:50 -0400

    making the Pengu fall when not touching the ground

--- platform-game/Pengu.java
+++ platform-game/Pengu.java
@@ -9,7 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Pengu extends Actor
 {
     private int speed = 7;
-    
+    private int vSpeed = 0;
+    private int acceleration = 2;
     /**
      * Act - do whatever the Pengu wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,6 +18,7 @@ public class Pengu extends Actor
     public void act() 
     {
         checkKeys();
+        checkFall();
     }
 
     private void checkKeys()
@@ -29,6 +31,32 @@ public class Pengu extends Actor
             setImage("pengu-right.png");
             moveRight();
         }
+    }
+    
+    public void checkFall()
+    {
+        if (onGround()) 
+        {
+            vSpeed = 0;
+        }
+        else
+        {
+            fall();
+        }
+    }
+   
+    public boolean onGround()
+    {
+        
+        
+        Actor under = getOneObjectAtOffset( 0, getImage().getHeight() / 2, Ground.class);
+        return under != null;
+    }
+ 
+    public void fall()
+    {
+        setLocation(getX(), getY() + speed);
+        vSpeed = vSpeed + acceleration;
     }
 
     public void moveRight()
